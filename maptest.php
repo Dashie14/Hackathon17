7042cac4030e038cc5d4e47438133ebfcee25ce7
Izzeddin Mojaddidi <adid1405@gmail.com>
2017-11-17 11:22:29 +0700

    Create maptest.php

---
+++ maptest.php
@@ -0,0 +1,4 @@
+<iframe height="80%" width="80%" src="//www.google.com/maps/embed/v1/place?q=Harrods,Brompton%20Rd,%20UK
+      &zoom=17
+      &key=AIzaSyBpF38hUqeQQeXBR2z2G92_Nz3jrFllfyQ">
+</iframe>
